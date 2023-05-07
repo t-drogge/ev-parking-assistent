88ce8c50bb3cc238ab17c55364eaf992ba86f541
Thorsten Drogge <tdrogge@outlook.de>
2023-05-07 17:46:21 +0200

    Added speech analysis

--- bot.js
+++ bot.js
@@ -3,15 +3,69 @@
 
 const { ActivityHandler, MessageFactory } = require('botbuilder');
 
+const { QnAMaker } = require('botbuilder-ai');
+const IntentRecognizer = require('./intentrecognizer');
+
+const { TextAnalyticsClient, AzureKeyCredential } = require("@azure/ai-text-analytics");
+
 class EchoBot extends ActivityHandler {
-    constructor() {
+    constructor(configuration, qnaOptions) {
         super();
+
+        //if (!configuration) throw new Error("[QnAMakerBot]: Missing configuration pararamter");
+        //this.qnaMaker = new QnAMaker(configuration.QnAConfiguration, qnaOptions);
+        
+        // create a LUIS connector
+        //this.intentRecognizer = new IntentRecognizer(configuration.LuisConfiguration);
+
+        const key = '7589e809ae8d4392bed182ec0dd736d0';
+        const endpoint = 'https://ev-parking-assistant-language.cognitiveservices.azure.com/';
+        const textAnalyticsClient = new TextAnalyticsClient(endpoint,  new AzureKeyCredential(key));
+
         // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
         this.onMessage(async (context, next) => {
-            const replyText = `EV Parking: ${ context.activity.text }`;
+            // Send user input to QnA Maker
+            //const qnaResults = await this.qnaMaker.getAnswers(context);
+/*
+            // send user input to LUIS
+            const LuisResult = await this.intentRecognizer.executeLuisQuery(context);
+
+            // Determine which service to respond with //
+            if (LuisResult.luisResult.prediction.topIntent === "findParking" &&
+                LuisResult.intents.findParking.score > .6 &&
+                LuisResult.entities.$instance && 
+                LuisResult.entities.$instance.location && 
+                LuisResult.entities.$instance.location[0]
+            ) {
+                const location = LuisResult.entities.$instance.location[0].text;
+                // call api with location entity info
+                const getLocationOfParking = "I found parking with a charging station at " + location;
+                console.log(getLocationOfParking)
+                await context.sendActivity(getLocationOfParking);
+                await next();
+                return;
+            }
+*/
+
+            const replyText = `Echo: ${ context.activity.text }`;
             await context.sendActivity(MessageFactory.text(replyText, replyText));
+
+            // Call the sentimentAnalysis function on the context text
+            await sentimentAnalysis(textAnalyticsClient,context.activity.text );
+
             // By calling next() you ensure that the next BotHandler is run.
             await next();
+
+            // If an answer was received from QnA Maker, send the answer back to the user.
+            //if (qnaResults[0]) {
+            //    console.log(qnaResults[0])
+            //    await context.sendActivity(`${qnaResults[0].answer}`);
+            //}
+            //else {
+                // If no answers were returned from QnA Maker, reply with help.
+            //    await context.sendActivity(`I'm not sure I can answer your question`
+            //    + 'I can find charging stations or electric vehicle parking'
+            //    + `Or you can ask me questions about electric vehicles`);            }
         });
 
         this.onMembersAdded(async (context, next) => {
@@ -25,6 +79,32 @@ class EchoBot extends ActivityHandler {
             // By calling next() you ensure that the next BotHandler is run.
             await next();
         });
+
+        // Create a function to call the text analytics analyzeSentiment method
+        async function sentimentAnalysis(client,userText){
+            console.log("Running sentiment analysis on: " + userText);
+            // Make userText into an array
+            const sentimentInput = [ userText ];
+            // call analyzeSentiment and get results
+            const sentimentResult = await client.analyzeSentiment(sentimentInput);
+            console.log("Got sentiment result");
+
+            // This is where you send the sentimentInput and sentimentResults to a database or storage instead of the console
+
+            sentimentResult.forEach(document => {
+                console.log(`ID: ${document.id}`);
+                console.log(`\tDocument Sentiment: ${document.sentiment}`);
+                console.log(`\tDocument Scores:`);
+                console.log(`\t\tPositive: ${document.confidenceScores.positive.toFixed(2)} \tNegative: ${document.confidenceScores.negative.toFixed(2)} \tNeutral: ${document.confidenceScores.neutral.toFixed(2)}`);
+                console.log(`\tSentences Sentiment(${document.sentences.length}):`);
+                document.sentences.forEach(sentence => {
+                    console.log(`\t\tSentence sentiment: ${sentence.sentiment}`)
+                    console.log(`\t\tSentences Scores:`);
+                    console.log(`\t\tPositive: ${sentence.confidenceScores.positive.toFixed(2)} \tNegative: ${sentence.confidenceScores.negative.toFixed(2)} \tNeutral: ${sentence.confidenceScores.neutral.toFixed(2)}`);
+                });
+            });
+        }
+
     }
 }
 
--- index.js
+++ index.js
@@ -60,8 +60,26 @@ const onTurnErrorHandler = async (context, error) => {
 // Set the onTurnError for the singleton CloudAdapter.
 adapter.onTurnError = onTurnErrorHandler;
 
+// Map configuration values values from .env file for each service
+const QnAConfiguration = {
+    knowledgeBaseId: process.env.QnAKnowledgebaseId,
+    endpointKey: process.env.QnAAuthKey,
+    host: process.env.QnAEndpointHostName
+};
+const LuisConfiguration = {
+    applicationId: process.env.LuisAppId,
+    endpointKey: process.env.LuisAPIKey,
+    endpoint: process.env.LuisAPIHostName
+};
+
+// Pass into configuration object
+const configuration = {
+    QnAConfiguration,
+    LuisConfiguration
+}
+
 // Create the main dialog.
-const myBot = new EchoBot();
+const myBot = new EchoBot(configuration);
 
 // Listen for incoming requests.
 server.post('/api/messages', async (req, res) => {
